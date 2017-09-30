dafc2a5af279bce58c0f68d7a4d25081c18a2747
Tiến Tài <tientai206@gmail.com>
2017-09-30 17:03:39 +0700

    Basic

--- application/views/site/head.php
+++ application/views/site/head.php
@@ -1,76 +1,136 @@
-<!--    FILE CSS-->
-<link rel="stylesheet" type="text/css" href="<?php echo public_url('site')?>/css/roboto.css">
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="<?php echo public_url()?>bootstrap/css/bootstrap.min.css">
-<!-- Latest compiled and minified JavaScript -->
-<link href="<?php echo public_url('site')?>/css/font-awesome.min.css" rel='stylesheet' type='text/css' media='all' />
-<link href="<?php echo public_url('site')?>/css/styles.css?v=90" rel='stylesheet' type='text/css' media='all' />
-<link href="<?php echo public_url('site')?>/css/default_style.min.css?v=90" rel='stylesheet' type='text/css' media='all' />
-<link href="<?php echo public_url('site')?>/design/theme-default/flexslider.css" rel='stylesheet'
-      type='text/css' media='all' />
+<head>
+    <link rel="shortcut icon" href="./theme.hstatic.net/1000177652/1000229231/14/favicon.png?v=90"
+          type="image/png"/>
+    <meta charset="utf-8"/>
+    <title>OxyWatch </title>
+    <link rel="canonical" href="http://happylive.vn/"/>
+    <meta name="robots" content="index,follow"/>
+    <meta name="revisit-after" content="1 day"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="HandheldFriendly" content="true">
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=0'
+          name='viewport'/>
+    <link rel="dns-prefetch" href="http://happylive.vn">
+    <link rel="dns-prefetch" href="./hstatic.net">
+    <script>
+        //<![CDATA[
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function () {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', './www.google-analytics.com/analytics.js', 'ga');
+        ga('create', 'UA-97086989-1', 'auto');
+        try {
 
+        } catch (e) {
+        }
+        ;
+        ga('send', 'pageview');
 
+        //]]>
+    </script>
+    <script>
+        //<![CDATA[
+        !function (f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function () {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window,
+            document, 'script', './connect.facebook.net/en_US/fbevents.js');
+        // Insert Your Facebook Pixel ID below.
+        fbq('init', '905600829489553');
+        fbq('track', 'PageView');
+        //]]>
+    </script>
+    <noscript>
+        <img height='1' width='1' style='display: none'
+             src='https://www.facebook.com/tr?id=905600829489553&amp;ev=PageView&amp;noscript=1'/></noscript>
+    <script type='text/javascript'>
+        //<![CDATA[
+        if ((typeof Haravan) === 'undefined') {
+            Haravan = {};
+        }
+        Haravan.culture = 'vi-VN';
+        Haravan.shop = 'happylive.myharavan.com';
+        Haravan.theme = {"name": "Theme mặc định", "id": 1000229231, "role": "main"};
+        Haravan.domain = 'happylive.vn';
+        //]]>
+    </script>
+    <script>
+        //<![CDATA[
+        (function () {
+            function asyncLoad() {
+                var urls = ["./hstatic.net/652/1000177652/20/2017/4-10/noname.js"];
+                for (var i = 0; i < urls.length; i++) {
+                    var s = document.createElement('script');
+                    s.type = 'text/javascript';
+                    s.async = true;
+                    s.src = urls[i];
+                    var x = document.getElementsByTagName('script')[0];
+                    x.parentNode.insertBefore(s, x);
+                }
+            }
 
-<!--    FILE JS-->
-<script src="<?php echo public_url()?>js/jquery-3.2.1.min.js"></script>
-<script src="<?php echo public_url()?>bootstrap/js/bootstrap.min.js"></script>
-<script src="<?php echo public_url('site')?>/js/option_selection.js" type='text/javascript'></script>
-<script src="<?php echo public_url('site')?>/js/api.jquery.js" type='text/javascript'></script>
-<script>
-    var formatMoney = '{{amount}}₫';
-</script>
-<script src="<?php echo public_url('site')?>/js/api.jquery.js?v=90" type='text/javascript'></script>
-<script src="<?php echo public_url('site')?>/design/theme-default/jquery-migrate-1.2.0.min.js"></script>
-<script src="<?php echo public_url('site')?>/design/theme-default/jquery.touchSwipe.min.js" type='text/javascript'></script>
-<script data-target=".product-resize" data-parent=".products-resize" data-img-box=".image-resize"
-        src="<?php echo public_url('site')?>/design/js/fixheightproductv2.js"></script>
-<script src="<?php echo public_url('site')?>/design/js/haravan.plugin.1.0.js"></script>
-<script src="<?php echo public_url('site')?>/design/theme-default/jquery.flexslider.js"
-        type='text/javascript'></script>
-
-<script src="<?php echo public_url('site')?>/design/14/scripts.js?v=90" type='text/javascript'></script>
-<script>
-    //<![CDATA[
-    (function (i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-            (i[r].q = i[r].q || []).push(arguments)
-        }, i[r].l = 1 * new Date(); a = s.createElement(o),
-            m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', './www.google-analytics.com/analytics.js', 'ga');
-    ga('create', 'UA-97086989-1', 'auto');
-    try {
-
-    } catch (e) { };
-    ga('send', 'pageview');
-
-    //]]>
-</script>
-<script>
-    //<![CDATA[
-    !function (f, b, e, v, n, t, s) {
-        if (f.fbq) return; n = f.fbq = function () {
-            n.callMethod ?
-                n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-        }; if (!f._fbq) f._fbq = n;
-        n.push = n; n.loaded = !0; n.version = '2.0'; n.queue = []; t = b.createElement(e); t.async = !0;
-        t.src = v; s = b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t, s)
-    } (window,
-        document, 'script', './connect.facebook.net/en_US/fbevents.js');
-    // Insert Your Facebook Pixel ID below.
-    fbq('init', '905600829489553');
-    fbq('track', 'PageView');
-    //]]>
-</script>
-<noscript>
-    <img height='1' width='1' style='display: none' src='https://www.facebook.com/tr?id=905600829489553&amp;ev=PageView&amp;noscript=1' /></noscript>
-<script type='text/javascript'>
-    //<![CDATA[
-    if ((typeof Haravan) === 'undefined') {
-        Haravan = {};
-    }
-    Haravan.culture = 'vi-VN';
-    Haravan.shop = 'happylive.myharavan.com';
-    Haravan.theme = { "name": "Theme mặc định", "id": 1000229231, "role": "main" };
-    Haravan.domain = 'happylive.vn';
-    //]]>
-</script>
+            window.attachEvent ? window.attachEvent('onload', asyncLoad) : window.addEventListener('load', asyncLoad, false);
+        })();
+        //]]>
+    </script>
+    <script src="./hstatic.net/0/0/global/design/js/jquery.min.1.11.0.js"></script>
+    <script src="./hstatic.net/0/0/global/design/js/bootstrap.min.js"></script>
+    <script src='./hstatic.net/0/0/global/option_selection.js' type='text/javascript'></script>
+    <script src='./hstatic.net/0/0/global/api.jquery.js' type='text/javascript'></script>
+    <script>
+        var formatMoney = '{{amount}}₫';
+    </script>
+    <script src='./theme.hstatic.net/1000177652/1000229231/14/default_script.min.js?v=90'
+            type='text/javascript'></script>
+    <link href='./theme.hstatic.net/1000177652/1000229231/14/default_style.min.css?v=90'
+          rel='stylesheet' type='text/css' media='all'/>
+    <!--[if lt IE 9]>
+    <script src="./hstatic.net/0/0/global/design/theme-default/html5shiv.js"></script>
+    <![endif]-->
+    <script src="./hstatic.net/0/0/global/design/theme-default/jquery-migrate-1.2.0.min.js"></script>
+    <script src='./hstatic.net/0/0/global/design/theme-default/jquery.touchSwipe.min.js'
+            type='text/javascript'></script>
+    <script data-target=".product-resize" data-parent=".products-resize" data-img-box=".image-resize"
+            src="./hstatic.net/0/0/global/design/js/fixheightproductv2.js"></script>
+    <script src="./hstatic.net/0/0/global/design/js/haravan.plugin.1.0.js"></script>
+    <script src='./hstatic.net/0/0/global/design/theme-default/jquery.flexslider.js'
+            type='text/javascript'></script>
+    <link href='./hstatic.net/0/0/global/design/theme-default/flexslider.css' rel='stylesheet'
+          type='text/css' media='all'/>
+    <script src='./theme.hstatic.net/1000177652/1000229231/14/scripts.js?v=90' type='text/javascript'></script>
+    <link rel="stylesheet" type="text/css" href="./hstatic.net/0/0/global/design/member/fonts-master/roboto.css">
+    <meta property="og:type" content="website"/>
+    <meta property="og:title" content="OxyWatch"/>
+    <meta property="og:image" content="http://theme.hstatic.net/1000177652/1000229231/14/share_fb_home.png?v=90"/>
+    <meta property="og:image" content="https://theme.hstatic.net/1000177652/1000229231/14/share_fb_home.png?v=90"/>
+    <meta property="og:url" content="http://happylive.vn/"/>
+    <meta property="og:site_name" content="Happylive"/>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="./hstatic.net/0/0/global/design/css/bootstrap.3.3.1.css">
+    <!-- Latest compiled and minified JavaScript -->
+    <link href='./hstatic.net/0/0/global/design/plugins/font-awesome/4.6.3/css/font-awesome.min.css'
+          rel='stylesheet' type='text/css' media='all'/>
+    <link href='./theme.hstatic.net/1000177652/1000229231/14/styles.css?v=90' rel='stylesheet'
+          type='text/css' media='all'/>
+</head>
